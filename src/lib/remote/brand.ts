ddb30f44dedc809a37b7ac0ce4745b4103f9e71a
atheimuz <atheimuz@naver.com>
2024-10-02 17:08:39 +0900

    refactor: axios 기본 url 설정

--- src/lib/remote/brand.ts
+++ src/lib/remote/brand.ts
@@ -1,15 +1,15 @@
-import axios from "axios";
+import client from "@/lib/remote/client";
 import { IBrand } from "@/models/brand";
 
 export const getBrandsAPI = async (brandId: string) => {
-    const res = await axios.get("/api/brands");
+    const res = await client.get("/api/brands");
     const data: { list: IBrand[] } = res.data;
 
     return data;
 };
 
 export const getBrandAPI = async (brandId: string) => {
-    const res = await axios.get(`/api/brand/${brandId}`);
+    const res = await client.get(`/api/brand/${brandId}`);
     const data: IBrand = res.data;
 
     return data;
--- src/lib/remote/menu.ts
+++ src/lib/remote/menu.ts
@@ -1,15 +1,15 @@
-import axios from "axios";
+import client from "@/lib/remote/client";
 import { IMenu } from "@/models/menu";
 
 export const getMenusAPI = async (query = {}) => {
-    const res = await axios.get("/api/menus", { params: query });
+    const res = await client.get("/api/menus", { params: query });
     const data: { list: IMenu[] } = res.data;
 
     return data;
 };
 
 export const getMenuAPI = async (menuId: string) => {
-    const res = await axios.get(`/api/menu/${menuId}`);
+    const res = await client.get(`/api/menu/${menuId}`);
     const data: IMenu = res.data;
 
     return data;
