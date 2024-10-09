72165f9262331cc2d9af2b53161d754467aa76f5
atheimuz <atheimuz@naver.com>
2024-10-10 01:38:31 +0900

    test: 테스트 console.log 추가

--- src/lib/remote/brand.ts
+++ src/lib/remote/brand.ts
@@ -1,9 +1,13 @@
 import { IBrand } from "@/models/brand";
 
 export const getBrandsAPI = async () => {
+    console.log("url:::", `${process.env.NEXT_PUBLIC_API_URI}/api/brands`);
     const res = await fetch(
         `${process.env.NEXT_PUBLIC_API_URI}/api/brands`
-    ).then((res) => res.json());
+    ).then((res) => {
+        console.log("res::::", res.text());
+        return res.json();
+    });
     const data: { list: IBrand[] } = res.data;
 
     return data;
