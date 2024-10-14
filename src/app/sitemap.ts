107682e6c97632f40cf8c5dec1aa3f3736ed3285
atheimuz <atheimuz@naver.com>
2024-10-14 18:52:37 +0900

    feat: sitemap, robots.txt 파일 추가

---
+++ src/app/sitemap.ts
@@ -0,0 +1,24 @@
+import { MetadataRoute } from "next";
+import { getBrandsAPI } from "@/lib/remote/brand";
+
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+    const data = await getBrandsAPI();
+    const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
+
+    const routes: MetadataRoute.Sitemap = [
+        {
+            url: `${baseUrl}/ff`,
+            lastModified: new Date()
+        }
+    ];
+
+    if ("list" in data) {
+        const brandRoutes: MetadataRoute.Sitemap = data.list.map((brand) => ({
+            url: `${baseUrl}/brand/${brand._id}`,
+            lastModified: new Date()
+        }));
+        routes.push(...brandRoutes);
+    }
+
+    return routes;
+}
