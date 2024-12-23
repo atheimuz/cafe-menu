1cb265b4857b760150f00780e74417c6d4247462
atheimuz <atheimuz@naver.com>
2024-12-23 19:48:40 +0900

    feat: 브랜드별 색상 지정

--- src/schema/brand.ts
+++ src/schema/brand.ts
@@ -8,3 +8,15 @@ export const brandLogoSrc: { [key: string]: string } = {
     mammothcoffee: "/images/brand/mammothcoffee.png",
     paikscoffee: "/images/brand/paikscoffee.png"
 };
+
+export const brandColor: { [key: string]: { primary: string; background: string; text: string } } =
+    {
+        starbucks: { primary: "#01704a", background: "#11a170", text: "#ffffff" },
+        twosomeplace: { primary: "#d70034", background: "#f42e5e", text: "#ffffff" },
+        hollys: { primary: "#da1425", background: "#ff4454", text: "#ffffff" },
+        ediya: { primary: "#14255c", background: "#384c92", text: "#ffffff" },
+        megacoffee: { primary: "#ffdc05", background: "#ffe231", text: "#442512" },
+        composecoffee: { primary: "#fed32c", background: "#fed32c", text: "#322625" },
+        mammothcoffee: { primary: "#20120e", background: "#6d453a", text: "#ffffff" },
+        paikscoffee: { primary: "#ffe908", background: "#ffee40", text: "#071f60" }
+    };
