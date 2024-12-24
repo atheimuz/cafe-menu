19c81c3e9254125d3dfce4be485aa498b751b4d8
atheimuz <atheimuz@naver.com>
2024-12-24 16:22:02 +0900

    feat: 뒤로가기 버튼 동작 변경

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -10,13 +10,17 @@ interface Props {
 const Header = ({ title }: Props) => {
     const router = useRouter();
 
+    const handleBack = () => {
+        if (document.referrer?.includes(window.location.origin)) {
+            router.back();
+        } else {
+            router.push("/");
+        }
+    };
+
     return (
         <header className={styles.wrapper}>
-            <button
-                type="button"
-                className={styles.btn}
-                onClick={() => router.back()}
-            >
+            <button type="button" className={styles.btn} onClick={handleBack}>
                 <CgChevronLeft />
             </button>
             <p className={styles.title}>{title}</p>
